d0a6ed9ea02c7ac5320aeae055c89ff71d4049f5
Avinash kumar K <avinashkumar.komera@cesltd.com>
2024-06-21 10:46:13 +0530

    Final commit to retrieve top N locations with longest daylight

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ const inputFilePath = path.join(__dirname, 'locations.json');
 
 // Transform stream to calculate sunlight duration
 class SunlightDurationTransform extends Transform {
-  constructor(date) {
+  constructor(date, topN) {
     super({ objectMode: true });
     this.date = date;
+    this.topN = topN
     this.places = [];
   }
 
@@ -22,27 +23,29 @@ class SunlightDurationTransform extends Transform {
       const sunset = times.sunset;
       const duration = (sunset - sunrise) / (1000 * 60 * 60); // duration in hours
 
-      this.places.push({ name, latitude, longitude, sunlightDuration: duration });
+      this.places.push({ name, latitude, longitude, sunlightDuration: parseFloat(duration.toFixed(2)) });
 
     callback();
   }
 
   _flush(callback) {
-    // Find the place with the longest sunlight duration
-    const longestDaylightPlace = this.places.reduce((max, place) => 
-      place.sunlightDuration > max.sunlightDuration ? place : max, this.places[0]);
+    // Sort places by sunlight duration in descending order
+    this.places.sort((a, b) => b.sunlightDuration - a.sunlightDuration);
 
-    console.log(`The location with the longest hours of sunlight on ${date.toDateString()} is ${longestDaylightPlace.name} with ${longestDaylightPlace.sunlightDuration.toFixed(2)} hours.`);
 
+    // Get the top N places
+    const topNPlaces = this.places.slice(0, this.topN);
+
+    console.log(`Top ${this.topN} places with the longest daylight duration:`, topNPlaces);
     callback();
   }
 }
 
 // Function to calculate and print the place with the longest daylight duration for a given date
-function findLongestDaylightPlace(date) {
+function findLongestDaylightPlace(date, topN) {
   // Create read stream
   const readStream = fs.createReadStream(inputFilePath, { encoding: 'utf8' });
-  const transformStream = new SunlightDurationTransform(date);
+  const transformStream = new SunlightDurationTransform(date, topN);
 
   //A streaming JSON parser can process large JSON data without loading the entire data into memory preventing syntax errors if the split occurs in the middle of a string or a structure.
   const jsonStream = JSONStream.parse('*');
@@ -61,4 +64,5 @@ function findLongestDaylightPlace(date) {
 
 // Example usage of the function
 const date = new Date("2024-03-02");
-findLongestDaylightPlace(date);
+const topN = 1;
+findLongestDaylightPlace(date, topN);
